07b4e4db6393045830a8c037219aa163a4842e4b
David <17435126+DavidLMS@users.noreply.github.com>
2025-04-26 09:38:09 +0200

    fix links to web

--- describepdf/ui.py
+++ describepdf/ui.py
@@ -175,7 +175,7 @@ def create_ui() -> gr.Blocks:
         gr.Markdown("<center><img src='https://davidlms.github.io/DescribePDF/assets/poster.png' alt='Describe PDF Logo' width='600px'/></center>")
         gr.Markdown(
             """<div style="display: flex;align-items: center;justify-content: center">
-            [<a href="https://davidlms.github.io/describepdf/">Project Page</a>] | [<a href="https://github.com/DavidLMS/describepdf">Github</a>]</div>
+            [<a href="https://davidlms.github.io/DescribePDF/">Project Page</a>] | [<a href="https://github.com/DavidLMS/describepdf">Github</a>]</div>
             """
         )
         gr.Markdown(
